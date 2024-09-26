fb309c27ad45c920f26dc25a1eb1098b4dcb0126
apna-colleage-demo <rahulmali2140@gmail.com>
2024-09-26 17:03:26 +0530

    first commit

---
+++ src/main/java/com/becoder/SpringSecurityRoleBaseAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.becoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRoleBaseAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRoleBaseAuthorizationApplication.class, args);
+	}
+
+}
